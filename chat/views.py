2fe1668a765d319513350dc1b8fb984dc8ba39c7
maksim.lazko <m.lazko@naveksoft.com>
2024-03-03 16:49:37 +0300

    init commit

---
+++ chat/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='chat/index.html')
+
+
+def room(request: HttpRequest, room_name: str) -> HttpResponse:
+    return render(request=request, template_name='chat/room.html', context={'room_name': room_name})
