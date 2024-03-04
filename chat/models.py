9c3d3f370f8a4b04e8070717dbdb3dddcef93f0d
maksim.lazko <m.lazko@naveksoft.com>
2024-03-04 13:56:18 +0300

    added msg model, updated consumers.py, added ui

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import QuerySet
 
-# Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_msgs', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.author.username
+
+    @classmethod
+    def last_10_messages(cls) -> QuerySet:
+        return cls.objects.order_by('-created_at').all()[:10]
